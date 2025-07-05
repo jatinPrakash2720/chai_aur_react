f81ca6e6784dbac4650041f87eb7f106a4c8803a
Jatin Prakash <jatin.prakash.cseaiml.2022@miet.ac.in>
2025-07-05 15:57:17 +0530

    learn useState hook properly

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css"
+import { useState } from "react"
+function App() {
+  // let counter = 5
+  const [counter, setCounter] = useState(15)
+  function addValue() {
+    // console.log(Math.random(), counter)
+    setCounter(counter + 1)
+  }
+  function removeValue() {
+    if (counter === 0) return
+    setCounter(counter - 1)
+  }
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>COunter value : {counter}</h2>
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button onClick={removeValue}> remove value {counter}</button>
+    </>
+  )
+}
+
+export default App
