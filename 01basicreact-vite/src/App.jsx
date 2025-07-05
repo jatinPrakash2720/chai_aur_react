f90e2947cada440080f3fd0bf8620aeb28f90ef1
Jatin Prakash <jatin.prakash.cseaiml.2022@miet.ac.in>
2025-07-05 15:31:23 +0530

    build my own customized reacts

--- 01basicreact-vite/src/App.jsx
+++ 01basicreact-vite/src/App.jsx
@@ -1,9 +1,13 @@
 import Chai from "./chai"
 function App() {
+  const username = "chai aur code"
   return (
-    <> //fragment
+    //fragment
+    <>
       <Chai />
-      <h1>chai aur react</h1>
+      <h1>chai aur react {username}</h1>
+      {/*evaluated javascript. hi likh skte hai,,, yani ki final outcome hi
+      likh skta hu*/}
       <p>Test para</p>
     </>
   )
--- 01basicreact-vite/src/main.jsx
+++ 01basicreact-vite/src/main.jsx
@@ -1,9 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.jsx"
+// import { jsx as _jsx } from "react/jsx-runtime.js"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return (
+    <div>
+      <h1>CustomApp ?</h1>
+    </div>
+  )
+}
+
+const ReactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+}
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit google
+  </a>
+)
+const anotherUser = "Chai aur code"
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  `${anotherUser} click me to visit google `
+)
+createRoot(document.getElementById("root")).render(
+  <App />
+  // reactElement
+  // anotherElement
+
+  // createRoot(document.getElementById("root")).render(
+  //   MyApp()
+  // ){/* <MyApp /> */}
 )
