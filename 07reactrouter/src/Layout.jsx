f798fcc29be5d7ed8cadc208a81a8925217e1396
Jatin Prakash <jatin.prakash.cseaiml.2022@miet.ac.in>
2025-07-16 12:00:06 +0530

    feat: Add layout structure with header and footer components
    feat: Create About component with descriptive content
    
    feat: Implement Contact component with a contact form
    
    feat: Add Footer component with navigation links and social media icons
    
    feat: Create Github component to display GitHub user data
    
    feat: Develop Header component with navigation links
    
    feat: Implement Home component with promotional content and images
    
    feat: Create User component to display user ID from URL parameters
    
    feat: Add index file for component exports
    
    style: Add Tailwind CSS configuration and import styles
    
    build: Set up Vite configuration for React and Tailwind CSS
    
    chore: Add image assets for the application

---
+++ 07reactrouter/src/Layout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header/Header.jsx"
+import Footer from "./components/Footer/Footer.jsx"
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+export { Layout }
