6411f35d24ad4a2b63ff2dea23c05b6b80c1fb23
Jatin Prakash <jatin.prakash.cseaiml.2022@miet.ac.in>
2025-07-21 12:07:34 +0530

    feat: initialize 12MegaBlog project with Vite, React, and Appwrite integration
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application layout and components.
    - Developed App.jsx as the main application component with a placeholder heading.
    - Added React logo SVG in the assets directory.
    - Configured Appwrite connection settings in conf.js.
    - Established global styles in index.css for consistent theming and layout.
    - Set up main.jsx for rendering the application using React's StrictMode.
    - Configured Vite with a basic setup in vite.config.js.

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+// import conf from "./conf/conf"
+
+function App() {
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
+  // console.log(conf.appwriteUrl)
+  return (
+    <>
+      <h1>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
